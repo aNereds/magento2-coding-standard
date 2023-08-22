10a27af5a3ce7c36053ebf654dbb9b80f5ebc4ee
Andzejs N <andzejs@maksekeskus.ee>
2023-08-22 19:22:06 +0300

    variable option add

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,10 @@
 #!/bin/sh
+echo "Checking file extensions: $1"
+echo "Display only warnings: $2"
+echo "Show Source report: $3"
 echo $GITHUB_WORKSPACE
 
 cd $GITHUB_WORKSPACE
 sh -c "/root/.composer/vendor/bin/phpcs -i"
-sh -c "/root/.composer/vendor/bin/phpcs --standard=Magento2 --extensions=php,phtml $GITHUB_WORKSPACE -s ./"
-sh -c "/root/.composer/vendor/bin/phpcs --standard=Magento2 --report=summary --extensions=php,phtml $GITHUB_WORKSPACE -s ./"
+sh -c "/root/.composer/vendor/bin/phpcs --standard=Magento2 --extensions=php,phtml $GITHUB_WORKSPACE ./"
+sh -c "/root/.composer/vendor/bin/phpcs --standard=Magento2 --report=summary --extensions=php,phtml $GITHUB_WORKSPACE ./"
