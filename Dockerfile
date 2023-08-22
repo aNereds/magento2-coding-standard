1f2b214f5217c0875499c0d9c9ad82ec040cce8c
Andzejs N <andzejs@maksekeskus.ee>
2023-08-22 17:14:20 +0300

    add correct path for composer execute

--- Dockerfile
+++ Dockerfile
@@ -15,5 +15,6 @@ RUN echo memory_limit = -1 >> /usr/local/etc/php/conf.d/custom-memory.ini
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
 
-# Code file to execute when the docker container starts up (`entrypoint.sh`)
+RUN ls -la
+
 ENTRYPOINT ["/entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -4,5 +4,5 @@ echo "Warning Severity $2"
 echo $GITHUB_WORKSPACE
 
 cd $GITHUB_WORKSPACE
-sh -c "~/.composer/vendor/bin/phpcs -i"
-sh -c "~/.composer/vendor/bin/phpcs --standard=Magento2 --error-severity=$1 --warning-severity=$2 $GITHUB_WORKSPACE -s ./"
+sh -c "/root/.composer/vendor/bin/phpcs -i"
+sh -c "/root/.composer/vendor/bin/phpcs --standard=Magento2 --error-severity=$1 --warning-severity=$2 $GITHUB_WORKSPACE -s ./"
